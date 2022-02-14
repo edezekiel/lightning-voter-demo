41fb4d0f324e4b385f4f85dc98cc326f9379847d
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2022-02-14 10:30:50 -0600

    Upgrade unreviewedTalk to component

--- public/home/unreviewedTalk.js
+++ public/home/unreviewedTalk.js
@@ -1,13 +1,10 @@
-angular.module('app').directive('unreviewedTalk', function() {
-  return {
+angular.module('app').component('unreviewedTalk', {
     templateUrl: '/home/unreviewedTalk.html',
-    scope: {
+    bindings: {
       session: '=',
       voteYes: '&',
       voteNo: '&'
     },
-    controllerAs: '$ctrl',
-    bindToController: true,
     controller: function() {
       
       this.yes = function() {
@@ -18,5 +15,4 @@ angular.module('app').directive('unreviewedTalk', function() {
         this.voteNo();
       }
     }
-  }
 })
