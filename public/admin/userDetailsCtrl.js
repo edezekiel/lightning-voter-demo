04ed193c662c3de038c33d6fb823b30466cb2fea
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2022-02-14 08:54:06 -0600

    Switch userDetails to component

--- public/admin/userDetailsCtrl.js
+++ public/admin/userDetailsCtrl.js
@@ -1,7 +1,11 @@
-angular.module('app').controller('userDetailsCtrl', 
-      function(allUsers, $routeParams) {
-  
-  this.user = allUsers.find(function(user) {
-    return user.id === parseInt($routeParams.id);
-  })
-})
+angular.module('app').component('userDetails', {
+  templateUrl: '/admin/userDetails.html',
+  bindings: {
+    allUsers: '='
+  },
+  controller: function ($routeParams) {
+    this.user = this.allUsers.find(function (user) {
+      return user.id === parseInt($routeParams.id);
+    });
+  },
+});
