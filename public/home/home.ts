1d27e24a5d0f0e39d4e02712b5ac3926a7072a45
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2022-02-16 12:50:15 -0600

    Remove sessions import from home component

--- public/home/home.ts
+++ public/home/home.ts
@@ -3,7 +3,7 @@ angular.module('app').component('home', {
   bindings: {
     userSessions: '='
   },
-  controller: function(currentIdentity, sessions, sessionsService, 
+  controller: function(currentIdentity, sessionsService, 
     toastr, unreviewedSessionCount) {
       
     
